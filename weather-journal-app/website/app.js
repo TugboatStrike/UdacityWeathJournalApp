54dfe3de61d573993c197c889973aea089fe58e8
Byron Meals <bmeals87@gmail.com>
2022-05-16 12:46:35 -0600

    Added OpenWeather API to app which is a bad place to keep this

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
 
+const apiKey = 'f41274d433dda89b92ea0f6ddd894eec';
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
